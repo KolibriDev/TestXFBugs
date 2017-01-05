62643ef40b4970abafca0f89e33041c726cb4d72
Hrafn Loftsson <hrafnl@islandsbanki.is>
2017-01-05 12:04:44 +0000

    Adds OnPropertyChanged events

--- TestXFBugs/LoginPageViewModel.cs
+++ TestXFBugs/LoginPageViewModel.cs
@@ -1,10 +1,14 @@
-﻿using Xamarin.Forms;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TestXFBugs.Annotations;
+using Xamarin.Forms;
 
 namespace TestXFBugs
 {
-    public class LoginPageViewModel
+    public class LoginPageViewModel : INotifyPropertyChanged
     {
         private bool _isKeyboardActive;
+        private LayoutOptions _scrollViewLayoutOptions;
 
         public LoginPageViewModel()
         {
@@ -20,11 +24,21 @@ namespace TestXFBugs
             set
             {
                 this._isKeyboardActive = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(InfoVisible));
                 this.SetScrollViewLayout(this._isKeyboardActive);
             }
         }
 
-        public LayoutOptions ScrollViewLayoutOptions { get; set; }
+        public LayoutOptions ScrollViewLayoutOptions
+        {
+            get { return this._scrollViewLayoutOptions; }
+            set
+            {
+                this._scrollViewLayoutOptions = value;
+                this.OnPropertyChanged();
+            }
+        }
 
         private void SetScrollViewLayout(bool keyboardActive)
         {
@@ -36,6 +50,14 @@ namespace TestXFBugs
             {
                 this.ScrollViewLayoutOptions = LayoutOptions.StartAndExpand;
             }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
